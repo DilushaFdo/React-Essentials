575d292c3f3257f67bdf32a9076a8034b93956e4
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2024-09-07 15:45:01 +0530

    Added Descriptions fo reach button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 
 import CoreConcepts from './components/CoreConcepts.jsx';
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 
 
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  function handleSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -23,6 +33,23 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[3]} />
 
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=> handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={()=> handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=> handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={()=> handleSelect('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+
         </section>
       </main>
     </div>
